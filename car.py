3a02df1293ce9342b036a3d0b84cdc6c9c1b0748
Prashant bk <prashantbk314@gmail.com>
2025-08-03 18:08:07 +0545

    added steering functionalities

--- car.py
+++ car.py
@@ -1,7 +1,9 @@
 # car structure(master)
 
 # car steering code (member 1)
-print("turn left")
-print("turn right")
+from steering.steering import left, right
+
+left()
+right()
 
 # car brakes code (member 2)
